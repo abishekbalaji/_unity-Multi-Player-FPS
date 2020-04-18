614f8df19597830f7421a1fe68dc9f5b25543fe3
abishek <abishekbalaji7.151@gmail.com>
2020-04-18 22:41:45 +0530

    Added player motor and control scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    private PlayerMotor motor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        //Final movement Vector
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+        //Applying movement
+        motor.Move(_velocity);
+
+    }
+}
